63323e0a34a8ebd6c62c0128a93355d48d8a66d8
EDIGAH01 <edigahclifford@gmail.com>
2023-08-10 15:59:29 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * Main  : Entry point
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf(size of a char: %d bytes(s)\n", sizeof(char);
+	printf(size of a int: %d bytes(s)\n", sizeof(int));
+	printf(size of a long int: %d bytes(s)\n", sizeof(lomg int));
+	printf(size of a long long int: %d bytes(s)\n", sizeof(long longint));
+	printf(size of a float: %d bytes(s)\n", sizeof(float));
+	return (0);
+}
