ef1f314e513c5348dd84044f899e0a7b39aff6d9
EDIGAH01 <edigahclifford@gmail.com>
2023-08-11 05:21:49 -0700

    edgar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
- * main  : Entry point
+ * main  - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -25,6 +25,6 @@ int main(void)
 			}
 		}
 	}
-	putchar("\n");
+	putchar('\n');
         return(0);
 } 
