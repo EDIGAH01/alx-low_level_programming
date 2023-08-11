82d3b062094e8e37c4ecb510694ac235f871a4d6
EDIGAH01 <edigahclifford@gmail.com>
2023-08-11 02:31:03 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main  : Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i > 0; i--)
+	{
+		putchar(alp[i]);
+	}
+	putchar("\n");
+
+        return(0);
+} 
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main  : Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	inti;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+
+        return(0);
+} 
