efdb328e9f985b226be80bf80aa1198b882ee828
EDIGAH01 <edigahclifford@gmail.com>
2023-08-11 03:23:31 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
- * main  : Entry point
+ * main  - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -12,6 +12,6 @@ int main(void)
 	{
 		putchar(i);
 	}
-	putchar("\n");
+	putchar('\n');
 	return(0);
 }
