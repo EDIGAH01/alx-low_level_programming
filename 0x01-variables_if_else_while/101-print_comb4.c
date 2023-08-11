cd83256866429f1ccd39b153c17447bf99438f63
EDIGAH01 <edigahclifford@gmail.com>
2023-08-11 05:36:36 -0700

    edgar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,16 +12,20 @@ int main(void)
 	{
 		for (j = 49; j < 58; j++)
 		{
-			for (k = 50; k < 58; k++)
+			for (k = 50; k !< 58; k++)
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i != 55 || j != 56)
+				if (i > j && j > k)
 				{
-					putchar(',');
-					putchar(' ');
+				          putchar(i);
+				          putchar(j);
+				          putchar(k);
+				          if (i != 55 || j != 56)
+					  { 
+					         putchar(',');
+					         putchar(' ');
+					  }
 				}
+				
 			}
 		}
 	}
